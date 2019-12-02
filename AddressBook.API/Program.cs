04f44f03668568cbc55f4b502c72240095025d47
imandzankovic <fruit.10@hotmail.com>
2019-12-02 01:19:15 +0100

    upgrade to 3

--- AddressBook.API/Program.cs
+++ AddressBook.API/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace AddressBook.API
@@ -14,11 +15,21 @@ namespace AddressBook.API
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+              /*  .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+
+                })*/
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- AddressBook.API/Startup.cs
+++ AddressBook.API/Startup.cs
@@ -4,53 +4,48 @@ using AddressBook.Repository;
 using AddressBook.Repository.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 namespace AddressBook.API
 {
     public class Startup
     {
-        public IConfigurationRoot Configuration { get; }
-
-        public Startup(IHostingEnvironment environment)
+       
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(environment.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables();
+            Configuration = configuration;
+        } 
+        public IConfiguration Configuration { get; }
+       
 
-            Configuration = builder.Build();
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "Boilerplate", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Boilerplate", Version = "v1" });
             });
-
-            var connection = Configuration.GetConnectionString("DefaultConnection");
+            var connection = "Server=tcp:contact-rg.database.windows.net,1433;Initial Catalog=AddressBook;Persist Security Info=False;User ID=contact;Password=address.123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             services.AddDbContext<ContactContext>(options =>
             options.UseSqlServer(connection, b => b.MigrationsAssembly("AddressBook.API")));
+            //services.AddControllers();
 
-            services.AddScoped<ContactRepository>();
-            services.AddScoped<IContactService, ContactService>();
-
+           
+           
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -60,6 +55,7 @@ namespace AddressBook.API
                 app.UseHsts();
             }
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
@@ -71,7 +67,9 @@ namespace AddressBook.API
             });
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            //app.UseMvc();
+
         }
+
     }
 }
